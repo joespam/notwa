ac933912d3eec418ba646ce1f9ff38747cb6c896
Joe Spampinato <jcspampinato@gmail.com>
2016-01-11 14:41:51 -0500

    fixes #8

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,7 +3,7 @@ class Profile < ActiveRecord::Base
 
 	# paperclip validations
 	#
-	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "missing.jpg"
 	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
 	# given a valid email address, set the
